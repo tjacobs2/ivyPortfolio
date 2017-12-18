1e99a0301770d488f9aab3547f37a0adc5c3f565
Tim Jacobs <timjacobs2@gmail.com>
2017-12-17 20:26:36 -0500

    Can now filter the list of commodoties from a list and then make a plot of the avg200 for each one

--- app.R
+++ app.R
@@ -31,70 +31,67 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+  #Set the detault set of ETFs
+  etfs <- data.frame(ETF = c("DBA", "DBC", "IFGL", "IYR", "TIP",
+                             "AGG", "IEMG", "IEFA", "IJR", "ITOT"),
+                     Type = c("Agricultural", "Commodities", "Intl Real Estate",
+                              "US Real Estate", "TIPs Bonds", "US Aggregated Bonds",
+                              "Emerging Markets", "Foreign Total", "S&P Small Cap",
+                              "S&P Total"),
+                     "Commission Free" = c("No", "No", "Yes", "No", "Yes",
+                                           "Yes", "Yes", "Yes", "Yes", "Yes"),
+                     check.names = FALSE)
+  
   #Show the user the ETFs we are using
   output$etfs <- renderDataTable({
     title <- "ETF Information"
-   
-    #Set the detault set of ETFs
-    etfs <- data.frame(ETF = c("DBA", "DBC", "IFGL", "IYR", "TIP",
-                               "AGG", "IEMG", "IEFA", "IJR", "ITOT"),
-                       Type = c("Agricultural", "Commodities", "Intl Real Estate",
-                                "US Real Estate", "TIPs Bonds", "US Aggregated Bonds",
-                                "Emerging Markets", "Foreign Total", "S&P Small Cap",
-                                "S&P Total"),
-                       "Commission Free" = c("No", "No", "Yes", "No", "Yes",
-                                             "Yes", "Yes", "Yes", "Yes", "Yes"),
-                       check.names = FALSE)
-    # print(etfs)
-    # print(etfs$ETF)
-    print(input$assetSelect)
-    # etfs <- etfs[c(input$assetSelect),]
-    print(etfs)
-    print(c(input$assetSelect))
-    # etfs <- subset[etfs, "ETF" == input$assetSelect]
+    etfs <- etfs[etfs$ETF %in% input$assetSelect, ]
   })
   
   #Add an observer to the fetch button that populates a data frame of ETF data.
   observeEvent(input$submit, {
-    
     # Loop through the collection of ETFs, fetch their data for the specified date range. This
     # seems like a very non-R way to do this, but it doesn't look like the tq_get function can take
     # multiple stock tickers. I also don't know how the most efficient way to iteratively add big
     # chunks to a Data Frame
+    etfs <- etfs[etfs$ETF %in% input$assetSelect, ]
     assetPriceHistory <- NULL
-    for (i in 1:length(etfs$ETF)) {
-      print(etfs$ETF[i])
-      curEtf <- tq_get(toString(etfs$ETF[i]),
-        get = "stock.prices",
-        from = input$dateRange[1],
-        to = input$dateRange[2]
-      )
-      curEtf$etf <- etfs$ETF[i]
-      curEtf$type <- etfs$Type[i]
-      #Add an column to the ETF data frame that is a 200 day average
-      curEtf <- tq_mutate(curEtf, select = adjusted,
-                                mutate_fun = rollapply, FUN = mean,
-                                width = 200, align = "right",
-                                col_rename = "avg200")
-      assetPriceHistory <- rbind(assetPriceHistory, curEtf)
-    }
-    
-    assetPriceHistory$position <- "invested"
-    assetPriceHistory[which(assetPriceHistory$adjusted <
-                              assetPriceHistory$avg200),]$position <- "cash"
-
-    print(assetPriceHistory)
-    output$assetPriceHistory <- renderPlot({
-      assetPriceHistory %>%
-        ggplot(aes(x = date, y = adjusted, color = etf)) +
-        geom_line() +
-        facet_wrap(~ type, scales = "free_y") +
-        ggtitle("Asset Clases Line Charts") +
-        ylab("Adjusted Closing Price") +
-        xlab("") +
-        theme_tq() +
-        theme(strip.text.x = element_text(size = 7))
-    })
+    withProgress(message = 'Making plot', value = 0, {
+      for (i in 1:length(etfs$ETF)) {
+        incProgress(1/length(etfs$ETF), detail = paste("Doing part", i))
+        print(etfs$ETF[i])
+        curEtf <- tq_get(toString(etfs$ETF[i]),
+          get = "stock.prices",
+          from = input$dateRange[1],
+          to = input$dateRange[2]
+        )
+        curEtf$etf <- etfs$ETF[i]
+        curEtf$type <- etfs$Type[i]
+        #Add an column to the ETF data frame that is a 200 day average
+        curEtf <- tq_mutate(curEtf, select = adjusted,
+                                  mutate_fun = rollapply, FUN = mean,
+                                  width = 200, align = "right",
+                                  col_rename = "avg200")
+        assetPriceHistory <- rbind(assetPriceHistory, curEtf)
+      }
+      
+      assetPriceHistory$position <- "invested"
+      assetPriceHistory[which(assetPriceHistory$adjusted <
+                                assetPriceHistory$avg200),]$position <- "cash"
+  
+      print(assetPriceHistory)
+      output$assetPriceHistory <- renderPlot({
+        assetPriceHistory %>%
+          ggplot(aes(x = date, y = adjusted, color = etf)) +
+          geom_line() +
+          facet_wrap(~ type, scales = "free_y") +
+          ggtitle("Asset Clases Line Charts") +
+          ylab("Adjusted Closing Price") +
+          xlab("") +
+          theme_tq() +
+          theme(strip.text.x = element_text(size = 7))
+      }) #Render
+    })# Progress Bar
   })
 }
 
